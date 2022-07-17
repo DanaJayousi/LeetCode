99efab09268329a8ab9d4651d0d1d012fb2d86bf
Dana <Danajayousi744@gmail.com>
2022-07-17 14:37:24 +0300

    add Running Sum of 1d Array

---
+++ LeetCode/RunningSumof1dArray.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode
+{
+    public class RunningSumof1dArray
+    {
+        public int[] RunningSum(int[] nums)
+        {
+            int[] sums = new int[nums.Length];
+            sums[0] = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                sums[i] = sums[i - 1] + nums[i];
+            }
+            return sums;
+        }
+    }
+}
