cbe2301e306e425c979271afdffbdb3cc7077f69
Dana <Danajayousi744@gmail.com>
2022-07-17 14:45:47 +0300

    add Rotate Array solution

---
+++ LeetCode/RotateArray.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode
+{
+    public class RotateArray
+    {
+        public void Rotate(int[] nums, int k)
+        {
+            int[] res = (int[])nums.Clone();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[(k + i) % nums.Length] = res[i];
+            }
+        }
+    }
+}
