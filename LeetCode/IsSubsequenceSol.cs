a468ebeec2482420338729751d79484ca9502bcb
Dana <Danajayousi744@gmail.com>
2022-07-17 14:42:07 +0300

    add Is Subsequence solution

---
+++ LeetCode/IsSubsequenceSol.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode
+{
+    public class IsSubsequenceSol
+    {
+        public bool IsSubsequence(string s, string t)
+        {
+            int i = 0;
+            int j = 0;
+            while (i < s.Length && j < t.Length)
+            {
+                if (s[i] == t[j])
+                {
+                    i++;
+                }
+                j++;
+            }
+            //Console.WriteLine($"i={i} and j={j}");
+            if (i < s.Length)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
